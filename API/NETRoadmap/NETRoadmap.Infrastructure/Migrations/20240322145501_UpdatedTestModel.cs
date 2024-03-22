c3827f92f58be38da3a3ead2a5d51f8cfdf86e5d
Maria Prylutska <63318704+sugaraddicted@users.noreply.github.com>
2024-03-22 16:01:34 +0100

    updated test model

---
+++ API/NETRoadmap/NETRoadmap.Infrastructure/Migrations/20240322145501_UpdatedTestModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NETRoadmap.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedTestModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Score",
+                table: "Tests");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Score",
+                table: "Tests",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
