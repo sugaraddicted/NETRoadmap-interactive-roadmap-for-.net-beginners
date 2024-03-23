f9e8c6c7647866094a677bb8fff7504b3f3acba1
Maria Prylutska <63318704+sugaraddicted@users.noreply.github.com>
2024-03-23 10:38:13 +0100

    add methods to retrieve topics, subtopics, tests, questions, and resources

--- API/NETRoadmap/NETRoadmap.Api/GraphQL/Query.cs
+++ API/NETRoadmap/NETRoadmap.Api/GraphQL/Query.cs
@@ -5,21 +5,63 @@ namespace NETRoadmap.Api.GraphQL
 {
     public class Query
     {
-        public async Task<Module> GetModuleById(Guid moduleId, [Service] IModuleService moduleService)
+        public async Task<Module> GetModuleById(Guid moduleId, 
+            [Service] IModuleService moduleService)
         {
             return await moduleService.GetByIdAsync(moduleId);
         }
-        public async Task<Topic> GetTopicById(Guid topicId, [Service] ITopicService topicService)
+
+        public async Task<Topic> GetTopicById(Guid topicId, 
+            [Service] ITopicService topicService)
         {
             return await topicService.GetByIdAsync(topicId);
         }
-        public async Task<Resource> GetResourceById(Guid resourceId, [Service] IResourceService resourceService)
+
+        public async Task<Resource> GetResourceById(Guid resourceId, 
+            [Service] IResourceService resourceService)
         {
             return await resourceService.GetByIdAsync(resourceId);
         }
-        public async Task<Test> GetTestById(Guid testId, [Service] ITestService testService)
+
+        public async Task<Test> GetTestById(Guid testId, 
+            [Service] ITestService testService)
         {
             return await testService.GetByIdAsync(testId);
+        }
+
+        public async Task<List<Topic>> GetTopicsByModuleId(Guid moduleId, 
+            [Service] ITopicService topicService)
+        {
+            return await topicService.GetTopicsByModuleIdAsync(moduleId);
+        }
+
+        public async Task<List<SubTopic>> GetSubTopicsByTopicId(Guid topicId, 
+            [Service] ISubTopicService subTopicService)
+        {
+            return await subTopicService.GetSubTopicsByTopicIdAsync(topicId);
+        }
+
+        public async Task<Test> GetTestByTopicId(Guid topicId, 
+            [Service] ITestService testService)
+        {
+            return await testService.GetTestByTopicIdAsync(topicId);
+        }
+
+        public async Task<List<Question>> GetQuestionsByTestId(Guid testId, 
+            [Service] IQuestionService questionService)
+        {
+            return await questionService.GetQuestionsByTestIdAsync(testId);
+        }
+
+        public async Task<List<Resource>> GetResourcesByTopicId(Guid topicId, 
+            [Service] IResourceService resourceService)
+        {
+            return await resourceService.GetResourcesByTopicIdAsync(topicId);
+        }
+        public async Task<List<SubResource>> GetSubResourcesByTopicId(Guid topicId, 
+            [Service] ISubResourceService subResourceService)
+        {
+            return await subResourceService.GetResourcesByTopicIdAsync(topicId);
         }
     }
 }
